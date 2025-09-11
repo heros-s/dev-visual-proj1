541910263f7160209c1219a952ab2647d258cd25
HEROS DE OLIVEIRA FREITAS DINAO <heros.dinao@cs.up.edu.br>
2025-09-10 21:19:17 -0300

    CRUD completo adicionado

--- FutebolApi/Program.cs
+++ FutebolApi/Program.cs
@@ -32,4 +32,32 @@ app.MapGet("/times/{id}", async (int id, AppDbContext db) =>
     return time is not null ? Results.Ok(time) : Results.NotFound("Time não encontrado.");
 });
 
+app.MapPost("/times", async (AppDbContext db, Time novoTime) => 
+{
+    db.Times.Add(novoTime);
+    await db.SaveChangesAsync();
+    return Results.Created($"Time {novoTime.Nome} adicionado com sucesso!", novoTime);
+});
+
+app.MapPut("/times/{id}", async (int id, AppDbContext db, Time timeAtualizado) => 
+{
+    var time = await db.Times.FindAsync(id);
+    if(time is null) return Results.NotFound("Time não encontrado.");
+    time.Nome = timeAtualizado.Nome;
+    time.Cidade = timeAtualizado.Cidade;
+    time.TitulosBrasileiros = timeAtualizado.TitulosBrasileiros;
+    time.TitulosMundiais = timeAtualizado.TitulosMundiais;
+    await db.SaveChangesAsync();
+    return Results.Ok(time);
+});
+
+app.MapDelete("/times/{id}", async (int id, AppDbContext db) => 
+{
+    var time = await db.Times.FindAsync(id);
+    if (time is null) return Results.NotFound("Time não encontrado!");
+    db.Times.Remove(time);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
 app.Run();
